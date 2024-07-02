4f274a1fe86ed46d15b35197d6395d929059c244
ekopinedaa <ekopineda@gmail.com>
2024-07-02 08:50:05 +0800

    Admin modification

--- parapo/src/pages/AdminDashboard.jsx
+++ parapo/src/pages/AdminDashboard.jsx
@@ -1,7 +1,30 @@
-import React from 'react';
-import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper } from '@mui/material';
+import React, { useState } from "react";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  Divider,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
+  Box,
+  IconButton,
+  Drawer,
+} from '@mui/material';
+import { useNavigate } from "react-router-dom";
+import LogoutIcon from "@mui/icons-material/Logout";
+import MenuIcon from "@mui/icons-material/Menu";
+
 
 const AdminDashboard = () => {
+  const navigate = useNavigate();
+  const [openDrawer, setOpenDrawer] = useState(false);
+
   // Sample user data (replace with actual data from API or state)
   const users = [
     {
@@ -27,8 +50,56 @@ const AdminDashboard = () => {
     // Add more user data as needed
   ];
 
+  const toggleDrawer = (open) => (event) => {
+    if (
+      event.type === "keydown" &&
+      (event.key === "Tab" || event.key === "Shift")
+    ) {
+      return;
+    }
+    setOpenDrawer(open);
+  };
+
+
+  const drawerContent = (
+    <Box
+      sx={{ width: 250 }}
+      role="presentation"
+      onClick={toggleDrawer(false)}
+      onKeyDown={toggleDrawer(false)}
+    >
+      <List>
+        <ListItem>
+          <ListItemText primary="Update Profile" />
+        </ListItem>
+      </List>
+      <Divider />
+      <List>
+        <ListItem button onClick={() => navigate("/")}>
+          <ListItemIcon>
+            <LogoutIcon />
+          </ListItemIcon>
+          <ListItemText primary="Logout" />
+        </ListItem>
+      </List>
+    </Box>
+  );
+
+
   return (
-    <div className="p-8 items-center justify-center w-screen ">
+    <div className="items-center justify-center w-screen ">
+      <div className="bg-customBlue fixed top-0 w-full h-[7rem] flex items-center text-customWhite">
+        <div className="p-8 justify-start">
+          <h1 className="text-roboto text-2xl font-bold flex">
+           Welcome! Care to Book a Ride?
+          </h1>
+        </div>
+        <div className="p-8 absolute right-0 items-center">
+          <IconButton onClick={toggleDrawer(true)}>
+            <MenuIcon className="text-customWhite" />
+          </IconButton>
+        </div>
+      </div>
       <h2 className="text-2xl font-bold mb-4">Admin Dashboard</h2>
       <Paper elevation={3}>
         <TableContainer>
@@ -62,6 +133,10 @@ const AdminDashboard = () => {
           </Table>
         </TableContainer>
       </Paper>
+
+      <Drawer anchor="right" open={openDrawer} onClose={toggleDrawer(false)}>
+        {drawerContent}
+      </Drawer>
     </div>
   );
 };
